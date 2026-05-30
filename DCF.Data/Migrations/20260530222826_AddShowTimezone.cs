a1dddadbdc8ee0e77813b81d150849948544751f
NeilNordhof <nnordhof@gmail.com>
2026-05-30 16:37:19 -0600

    feat: AddShowTimezone migration

---
+++ DCF.Data/Migrations/20260530222826_AddShowTimezone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DCF.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShowTimezone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Timezone",
+                table: "Shows",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Timezone",
+                table: "Shows");
+        }
+    }
+}
